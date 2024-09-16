91bb532b188ba15c28a9454076689793221d359b
Nikhilgorle <nikhil.gorle2002@gmail.com>
2024-09-16 18:13:22 +0530

    news website using class component-first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    let {title,description,imageUrl,newsUrl}=this.props;
+    return (
+      <div className='my-3'>
+        <div className="card" style={{width:"18rem"}}>
+            <img src={!imageUrl?"https://techreport.com/wp-content/uploads/2024/09/judge-3665164_1280.jpg?_t=1726357644":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sn btn-dark">Read More</a>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
